234dd1ed399e48dee14ab91e751fa718006db5ce
Kemal Çetin <73641701+KemalCetin01@users.noreply.github.com>
2025-08-06 16:55:27 +0300

    fix

--- src/Core/ModuleTech.Application/DTOs/Identity/Response/CreateIdentityBusinessResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿using ModuleTech.Core.Base.Dtos;
-
-namespace ModuleTech.Application.DTOs.Identity.Response;
-public class CreateIdentityBusinessResponseDto : IResponse
-{
-    public bool IsSuccess { get; set; }
-    public Guid IdentityRefId { get; set; }
-}
--- src/Core/ModuleTech.Application/Handlers/B2BRoles/Queries/SearchBusinessRolesQueryFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using ModuleTech.Core.Base.Handlers.Search;
-
-namespace ModuleTech.Application.Handlers.B2bRoles.Queries;
-
-public class SearchBusinessRolesQueryFilter : IFilter
-{
-    public Guid BusinessId { get; set; }
-    public string name { get; set; }
-}
